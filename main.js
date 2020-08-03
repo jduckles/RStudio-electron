fb1ee252d52be7823e791982c64eadb4bd0c0fd5
Jonah Duckles <jonah@duckles.org>
2020-08-04 09:14:05 +1200

    Updating for my use with RStudio

--- main.js
+++ main.js
@@ -5,15 +5,15 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1024,
+    height: 768,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadURL('https://rstudio.duckles.nz')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -24,7 +24,7 @@ function createWindow () {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
-  
+
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
